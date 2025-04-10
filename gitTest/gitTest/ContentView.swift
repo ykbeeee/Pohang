ce342ee6f73568c6a6f0e0c5a419a979d1e3356a
Libby <hiykb01@icloud.com>
2025-04-10 20:13:33 +0900

    Update ContentView.swift

--- gitTest/gitTest/ContentView.swift
+++ gitTest/gitTest/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
             Text("고양이")
             Text("고양이")
 
