7923f6f91e8beeda7aaa0e750ca6b935ac5de112
Libby <hiykb01@icloud.com>
2025-04-10 19:57:07 +0900

    project created

---
+++ gitTest/gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Libby Bae on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
